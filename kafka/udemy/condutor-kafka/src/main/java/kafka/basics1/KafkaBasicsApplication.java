0f16da5001750687fcc59ad53bc5df77008311f2
BisvarupMukherjee <bisvarup.mukherjee@uber.com>
2022-08-15 14:03:11 +0530

    feat: created scaffold of kafka producer

---
+++ kafka/udemy/condutor-kafka/src/main/java/kafka/basics1/KafkaBasicsApplication.java
@@ -0,0 +1,13 @@
+package kafka.basics1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KafkaBasicsApplication {
+    public static final Logger log = LoggerFactory.getLogger(KafkaBasicsApplication.class.getName());
+    public static void main(String[] args) {
+        log.info("Starting producer");
+
+
+    }
+}
