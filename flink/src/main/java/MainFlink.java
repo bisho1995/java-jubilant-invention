d5f8b2e3b51a47b20f7c2412e0ba3aa9f22ba458
BisvarupMukherjee <bisvarup.mukherjee@uber.com>
2022-08-25 17:54:09 +0530

    feat: did flink poc

--- flink/src/main/java/MainFlink.java
+++ flink/src/main/java/MainFlink.java
@@ -1,5 +1,94 @@
+import com.google.gson.annotations.SerializedName;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
+import org.apache.flink.streaming.api.functions.windowing.AllWindowFunction;
+import org.apache.flink.streaming.api.watermark.Watermark;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 public class MainFlink {
-    public static void main(String[] args) {
-        System.out.println("Working");
+
+    @Builder
+    @Getter
+    @Setter
+    @ToString
+    public static class Payload {
+        @SerializedName("Barrier_Free")
+        private final String barrier_free = "No";
+
+        @SerializedName("id")
+        private final String id;
+    }
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+        environment.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+        Payload p1 = Payload.builder().id("1").build();
+        Payload p2 = Payload.builder().id("2").build();
+
+        environment
+                .fromElements(p1,p2)
+                .assignTimestampsAndWatermarks(new AssignerWithPeriodicWatermarks<Payload>() {
+                    private long timestamp = 0;
+                    @Nullable
+                    @Override
+                    public Watermark getCurrentWatermark() {
+                        return new Watermark(timestamp);
+                    }
+
+                    @Override
+                    public long extractTimestamp(Payload payload, long l) {
+                        timestamp = System.currentTimeMillis();
+
+                        return timestamp;
+                    }
+                })
+//                .assignTimestampsAndWatermarks(new AssignerWithPeriodicWatermarks<Payload>() {
+//                    private long timestamp = 0;
+//
+//                    @Nullable
+//                    @Override
+//                    public Watermark getCurrentWatermark() {
+//                        return new Watermark(timestamp);
+//                    }
+//
+//                    @Override
+//                    public long extractTimestamp(
+//                            Integer germanyDriverLocationPayload, long l) {
+//                        timestamp = System.currentTimeMillis();
+//
+//                        return timestamp;
+//                    }
+//                })
+                .name(
+                        "assigned-timestamps-and-watermarks")
+                .windowAll(TumblingEventTimeWindows.of(Time.seconds(2)))
+                .apply(new AllWindowFunction<Payload, List<Payload>, TimeWindow>() {
+                    @Override
+                    public void apply(TimeWindow timeWindow, Iterable<Payload> iterable, Collector<List<Payload>> collector) throws Exception {
+                        List<Payload> list = new ArrayList<>();
+                        for(Payload p: iterable) {
+                            list.add(p);
+                        }
+
+                        collector.collect(list);
+                    }
+                })
+               .print();
+
+        environment.execute();
     }
 }
---
+++ random-practice/src/main/java/t1/F1.java
@@ -0,0 +1,5 @@
+package t1;
+
+public class F1 {
+    private static class C1 {}
+}
