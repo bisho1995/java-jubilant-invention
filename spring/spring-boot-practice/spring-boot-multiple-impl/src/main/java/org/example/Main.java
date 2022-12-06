e2903c8cfef51575176d025ac5e647026d304d31
BisvarupMukherjee <bisvarup.mukherjee@uber.com>
2022-12-06 11:54:00 +0530

    feat: turning off banners

--- spring/spring-boot-practice/spring-boot-multiple-impl/src/main/java/org/example/Main.java
+++ spring/spring-boot-practice/spring-boot-multiple-impl/src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(Main.class);
+        application.setBannerMode(Banner.Mode.OFF);
         application.run(args);
     }
 }
