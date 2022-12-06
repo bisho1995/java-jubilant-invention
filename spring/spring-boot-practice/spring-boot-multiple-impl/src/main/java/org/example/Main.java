2da2c5c498bba093793fe88cca4e49e81dbc276e
BisvarupMukherjee <bisvarup.mukherjee@uber.com>
2022-12-06 11:48:40 +0530

    feat: breaking SpringApplication

--- spring/spring-boot-practice/spring-boot-multiple-impl/src/main/java/org/example/Main.java
+++ spring/spring-boot-practice/spring-boot-multiple-impl/src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class,args);
+        SpringApplication application = new SpringApplication(Main.class);
+        application.run(args);
     }
 }
