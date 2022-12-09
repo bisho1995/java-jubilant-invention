4d4503cc56dcf0d1bf073e4a0e8d787eb978b17f
BisvarupMukherjee <bisvarup.mukherjee@uber.com>
2022-12-09 21:51:45 +0530

    feat: added more comments

--- spring/spring-boot-practice/spring-boot-configuration-and-beans/src/main/java/org/example/configurations/Configuration1.java
+++ spring/spring-boot-practice/spring-boot-configuration-and-beans/src/main/java/org/example/configurations/Configuration1.java
@@ -1,12 +1,13 @@
 package org.example.configurations;
 
 import org.example.beans.Bean1;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration // Tags the class as a source of bean definitions for the application context
 public class Configuration1 {
-    @Bean
+    @Bean // The bean is instantiated in eager instantiation way & kept ready
     public Bean1 myBean1(){
         return new Bean1();
     }
